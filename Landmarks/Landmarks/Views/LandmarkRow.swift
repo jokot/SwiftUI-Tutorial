dd5ef1014071c31b6fbedfce6774fd6ed4ff71b7
Joko Triyanto <jokotriyanto12345@gmail.com>
2024-04-29 09:13:47 +0700

    feat: Create the row view

---
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,26 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Jokot  on 29/04/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}
