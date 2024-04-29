b57dc1481aae1e9de576760e22edc5809f4de387
Joko Triyanto <jokotriyanto12345@gmail.com>
2024-04-29 09:21:05 +0700

    feat: create the list of landmarks

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Jokot  on 29/04/24.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
