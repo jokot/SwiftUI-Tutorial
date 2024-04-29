37d80e06198548c6693db4eca06183d3f67b9b00
Joko Triyanto <jokotriyanto12345@gmail.com>
2024-04-29 08:34:08 +0700

    feat: creating and combining views

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jokot  on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
