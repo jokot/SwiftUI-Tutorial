6fed805039de21e9ca931fdb35fbc6804d48ae3d
Joko Triyanto <jokotriyanto12345@gmail.com>
2024-05-01 16:44:19 +0700

    feat: draw the badge background

--- Landmarks/Landmarks/Views/HexagonParameters.swift
+++ Landmarks/Landmarks/Views/HexagonParameters.swift
@@ -19,7 +19,7 @@ struct HexagonParameters {
     static let segments = [
         Segment(
             line:    CGPoint(x: 0.60, y: 0.05),
-            curve:   CGPoint(x: 0.50, y: 0.05),
+            curve:   CGPoint(x: 0.40, y: 0.05),
             control: CGPoint(x: 0.50, y: 0.00)
         ),
         Segment(
